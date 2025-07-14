a0b0be36523b5b0563fd30a088ebb4437f4694b9
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-07-14 23:54:25 +0100

    Make dropdown menu functional

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./global-styles.css";
+
+const dropDownButton = document.querySelector(".dots");
+const dropDownMenu = document.querySelector(".menu");
+
+dropDownButton.addEventListener("click", function () {
+  if (dropDownMenu.style.display === "none") {
+    dropDownMenu.setAttribute("style", "display: block");
+    dropDownMenu.dataset.open = true;
+  } else {
+    dropDownMenu.setAttribute("style", "display: none");
+    dropDownMenu.dataset.open = false;
+  }
+});
+window.addEventListener("click", function (e) {
+  if (
+    !dropDownMenu.contains(e.target) &&
+    !dropDownButton.contains(e.target) &&
+    dropDownMenu.dataset.open === "true"
+  ) {
+    dropDownMenu.setAttribute("style", "display: none");
+    dropDownMenu.dataset.open = false;
+  } else {
+    return;
+  }
+});
